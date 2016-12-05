208d662460d0363bdbbbb865e6415276ccff42e2
h3xm4s7er <f12c6b9@gmail.com>
2016-12-05 06:30:07 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import time
+
+def main():
+	os.system("clear")
+	print("[+] Installing h3xNet...")
+	time.sleep(9)
+	os.system("git clone H3XNet.py")
+	time.sleep(3)
+	print("")
+	print("[+] Running first-time-config...")
+	time.sleep(3)
+	print("")
+	sys.exit()
+
+main()
